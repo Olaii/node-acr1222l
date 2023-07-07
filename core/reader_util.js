e657148e7c3c1f682fa94524411341db30860cbc
Klemen Kastelic <klemen@olaii.com>
2023-07-07 13:36:01 +0200

    - worked on reader_util

--- core/reader_util.js
+++ core/reader_util.js
@@ -5,12 +5,22 @@ const isOsx = /^darwin/.test(process.platform);
 
 
 const service = {
-
   // Define correct CTRL_PROTOCOL to be used when establishing a connection
   CTRL_PROTOCOL: (isWin ? 0 : 4),  // 0 for Windows, 4 for macOS and Linux
   CARD_PROTOCOL: 3,  // T1 or T0 - card will decide.
-
-  CONN_MODE: function (reader) { return isWin ? reader.SCARD_SHARE_SHARED : reader.SCARD_SHARE_DIRECT },
+  CONN_MODE: function (reader) {
+    return isWin ? reader.SCARD_SHARE_SHARED : reader.SCARD_SHARE_DIRECT
+  },
+  // Reader Commands
+  CMD_BACKLIGHT_ON: Buffer.from([0xFF, 0x00, 0x64, 0xFF, 0x00]),
+  CMD_BACKLIGHT_OFF: Buffer.from([0xFF, 0x00, 0x64, 0x00, 0x00]),
+  CMD_READ_UUID: Buffer.from([0xFF, 0xCA, 0x00, 0x00, 0x00]),
+  CMD_WRITE: function (buffer, addr) {
+    return Buffer.from([0xFF, 0xD6, 0x00, addr, buffer.length, ...buffer]);
+  },
+  CMD_READ_BYTES: function (addr, num_bytes) {
+    return Buffer.from([0xFF, 0xB0, 0x00, addr, num_bytes]);
+  },
 
   /**
    * ACR1222L presents itself as 4 separate readers to the OS, because of the SAM
@@ -21,7 +31,7 @@ const service = {
    *
    * @param reader
    * @returns {boolean}
-   */
+  */
   isValidReader: function (reader) {
     if (isWin) {
       return reader.name.toLowerCase().indexOf('picc 0') > -1
@@ -33,25 +43,14 @@ const service = {
   },
 
 
-  // Reader Commands
-  CMD_BACKLIGHT_ON: Buffer.from([0xFF, 0x00, 0x64, 0xFF, 0x00]),
-  CMD_BACKLIGHT_OFF: Buffer.from([0xFF, 0x00, 0x64, 0x00, 0x00]),
-  CMD_READ_UUID: Buffer.from([0xFF, 0xCA, 0x00, 0x00, 0x00]),
-
-  CMD_WRITE: function (buffer, addr) { return Buffer.from([0xFF, 0xD6, 0x00, addr, buffer.length, ...buffer]); },
-
-  CMD_READ_BYTES: function (addr, num_bytes) { return Buffer.from([0xFF, 0xB0, 0x00, addr, num_bytes]) },
-
-
   getLCDTextCmd: function (text, row_num = 1) {
     text = text + ' '.repeat(Math.max(16 - text.length, 0)); // fill in to be of length 16
     const txt = Buffer.from(text);
     const len = Buffer.from([text.length]);
-
     const row_cmd = row_num === 1 ? 0x00 : 0x40;
-
     return Buffer.concat([Buffer.from([0xFF, 0x00, 0x68, row_cmd]), len, txt]);
   },
+
 
   performCardPresentCallbacks: function (waitingRequests) {
     for (let key in waitingRequests) {
@@ -74,20 +73,23 @@ const service = {
     }
   },
 
+
   wrapCmd: function (cmd, dataIn) {
-    /*{
-     0xFF, //CLA
-     0x00, //INS
-     0x00, //P1
-     0x00, //P2
-     0x07, //LC // - total lenght  with D4h and 42h
-     0xD4, 0x42, //InCommunicateThru
-     0x1B, //PWD_AUTH (See data sheet) - CMD here
-     Password,  // payload - instruction data
-     Password >> 8,
-     Password >> 16,
-     Password >> 24,
-     };*/
+    /**
+    {
+      0xFF, // CLA
+      0x00, // INS
+      0x00, // P1
+      0x00, // P2
+      0x07, // LC // - total lenght with D4h and 42h
+      0xD4, 0x42, // InCommunicateThru
+      0x1B, // PWD_AUTH (See data sheet) - CMD here
+      Password, // payload - instruction data
+      Password >> 8,
+      Password >> 16,
+      Password >> 24,
+    };
+    */
     return Buffer.from([0xFF, 0x00, 0x00, 0x00, dataIn.length + 3, 0xD4, 0x42, cmd, ...dataIn]);
   },
 
@@ -121,13 +123,9 @@ const service = {
           };
         }
       }
-
       throw new Error('Unknown NDEF message');
     }
-
-
   }
-
 };
 
 
